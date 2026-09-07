24306f881cab4d2937f701c2e495aaaa05d98d4f
prog0ld <progold1324@gmail.com>
2026-09-07 15:46:29 +0300

    Fix BlockProtect.kt imports and use correct Category enum

--- src/main/kotlin/dev/goldaddons/modules/BlockProtect.kt
+++ src/main/kotlin/dev/goldaddons/modules/BlockProtect.kt
@@ -1,10 +1,10 @@
 package dev.goldaddons.modules
 
-import foo.starred.athen.
+import foo.starred.athen.config.Category
 import foo.starred.athen.modules.Module
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents
 
-class BlockProtect : Module("BlockProtect", "Prevents breaking protected blocks", Category.WORLD) {
+class BlockProtect : Module("BlockProtect", "Prevents breaking protected blocks", Category.GENERAL) {
 
     init {
         // Register the pre-break listener directly with Fabric API
