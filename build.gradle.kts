d5057e7b2028e860bf1219dce9f89e24ed9bd7d4
prog0ld <progold1324@gmail.com>
2026-09-07 15:48:39 +0300

    Update build.gradle to try maven first, with fallback to local JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,11 @@ repositories {
 	flatDir {
 		dirs("libs")
 	}
+	
+	// Try maven repositories first
 	maven("https://maven.starred.foo/releases")
 	maven("https://maven.starred.foo/snapshots")
+	mavenCentral()
 }
 
 dependencies {
@@ -32,8 +35,13 @@ dependencies {
 	implementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
 	implementation("net.fabricmc:fabric-language-kotlin:${providers.gradleProperty("fabric_kotlin_version").get()}")
 
-	// Athen dependency - includes the JAR from libs directory
-	implementation(files("libs/athen.jar"))
+	// Try to get Athen from maven first, fall back to local JAR if not found
+	try {
+		implementation("foo.starred:athen:0.3.2b")
+	} catch (e: Exception) {
+		// If maven fails, use local JAR
+		implementation(files("libs/athen.jar"))
+	}
 
 	implementation("maven.modrinth:avbpWn0t:R4TvAZLp")
 }
